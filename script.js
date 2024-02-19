c99d092052dcd510e6953da173d8897ccac6694e
anubhavjha893 <anubhawjha893@gmail.com>
2024-02-19 19:04:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var img = document.querySelector("img")
+var i = document.querySelector("i")
+img.addEventListener("dblclick",function(){
+    i.style.transform = "translate(-50%,-50%) scale(1)"
+    i.style.opacity = 0.8
+    setTimeout(function() {
+        i.style.opacity = 0;
+    }, 1000);
+    setTimeout(function() {
+        i.style.transform = "translate(-50%,-50% )scale(0)";
+    }, 2000);
+
+})
